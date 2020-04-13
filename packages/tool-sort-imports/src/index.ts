014a34d74e42194a82cd3f4eb10ce56ccd916201
lemon-clown <me.clown.lemon@gmail.com>
2020-04-13 20:24:47 +0800

    :construction:  [tool-sort-imports] feat: support command cli

--- packages/tool-sort-imports/src/index.ts
+++ packages/tool-sort-imports/src/index.ts
@@ -1,26 +1,32 @@
-import { createStaticImportOrExportRegex } from './util'
+import {
+  StaticImportOrExportStatItem,
+  compareModulePath,
+  createStaticImportOrExportRegex,
+  formatImportOrExportStatItem,
+} from './util'
+export * from './util'
 
 
-interface StaticImportStatItem {
-  type: 'import' | 'export'
-  moduleName: string
-  fullStatement: string
-  defaultExport?: string
-  exportN?: string[]
-}
-
-
-class StaticImportStatement {
+export class StaticImportStatement {
+  public readonly quote: string
   public readonly indent: string
   public readonly maxColumn: number
-  public readonly itemRank: Record<StaticImportStatItem['type'], number>
+  public readonly itemRank: Record<StaticImportOrExportStatItem['type'], number>
   public readonly staticImportOrExportRegex: RegExp
 
   public constructor(
+    quote = '\'',
     indent = '  ',
     maxColumn = 100,
-    staticImportOrExportRegex: RegExp = createStaticImportOrExportRegex()
+    staticImportOrExportRegex: RegExp = createStaticImportOrExportRegex('g')
   ) {
+    if (staticImportOrExportRegex.flags.indexOf('g') < 0) {
+      // eslint-disable-next-line no-param-reassign
+      staticImportOrExportRegex = new RegExp(
+        staticImportOrExportRegex, staticImportOrExportRegex.flags + 'g')
+    }
+
+    this.quote = quote
     this.indent = indent
     this.maxColumn = maxColumn
     this.staticImportOrExportRegex = staticImportOrExportRegex
@@ -30,60 +36,45 @@ class StaticImportStatement {
     }
   }
 
-  public handle(content: string): string {
+  public process(content: string): string {
     const self = this
-    const [endIndex, items] = self.collect(content)
+    const items: StaticImportOrExportStatItem[] = []
+    const regex: RegExp = this.staticImportOrExportRegex
+    regex.lastIndex = 0
+    let startIndex = 0
+    for (let m: RegExpExecArray | null; (m = regex.exec(content)) != null;) {
+      if (!/^[;\s]*$/.test(content.substring(startIndex, m.index))) break
+      const { defaultExport, exportN: exportNStr, moduleName, type } = m.groups!
+      const exportN: string[] = exportNStr == null ? [] : exportNStr.split(/\s*,\s*/g)
+      const item = {
+        defaultExport,
+        exportN: exportN.filter(x => /\S/.test(x)).sort(),
+        moduleName,
+        type: type as any,
+      } as StaticImportOrExportStatItem
+      item.fullStatement = self.format(item)
+      items.push(item)
+      startIndex = regex.lastIndex
+    }
+    if (startIndex <= 0) return content.trimLeft()
+
     const newStatements: string = items
-      .map(item => {
-        const fullStatement = self.format(item)
-        return { ...item, fullStatement }
-      })
-      .sort(self.compare)
+      .sort(self.compare.bind(self))
       .map(item => item.fullStatement).join('\n')
-    return newStatements + '\n\n\n' + content.slice(endIndex)
+    return (newStatements + '\n\n\n' + content.slice(startIndex).trimLeft()).trimRight() + '\n'
   }
 
-  protected collect(content: string): [number, Omit<StaticImportStatItem, 'fullStatement'>[]] {
-    const items: Omit<StaticImportStatItem, 'fullStatement'>[] = []
-    return [-1, items]
+  protected format(item: Omit<StaticImportOrExportStatItem, 'fullStatement'>): string {
+    const { quote, indent, maxColumn } = this
+    return formatImportOrExportStatItem(item, quote, indent, maxColumn)
   }
 
-  protected format(item: Omit<StaticImportStatItem, 'fullStatement'>): string {
-    const self = this
-    const assembleStatement = (multiline: boolean): string => {
-      let result: string = item.type
-      if (item.defaultExport || item.exportN) {
-        if (item.defaultExport) {
-          result += ' ' + item.defaultExport
-          if (item.exportN) result += ','
-        }
-        if (item.exportN) {
-          if (multiline) {
-            result += ' {\n' + item.exportN.map(x => self.indent + x + ',').join('\n') + '\n}'
-          } else {
-            result += ` { ${ item.exportN.join(', ') } }`
-          }
-        }
-        result += ' from'
-      }
-      result += ` '${ item.moduleName }'`
-      return result
-    }
-
-    let result = assembleStatement(false)
-    if (result.length >= this.maxColumn && item.exportN) {
-      // 将花括号中的 export 拆成多行
-      result = assembleStatement(true)
-    }
-    return result
-  }
-
-  protected compare(x: StaticImportStatItem, y: StaticImportStatItem): number {
+  protected compare(x: StaticImportOrExportStatItem, y: StaticImportOrExportStatItem): number {
     if (x.type !== y.type) {
       return this.itemRank[x.type] - this.itemRank[y.type]
     }
     if (x.moduleName !== y.moduleName) {
-      return x.moduleName < y.moduleName ? -1 : 1
+      return compareModulePath(x.moduleName, y.moduleName)
     }
     if (x.fullStatement === y.fullStatement) return 0
     return x.fullStatement < y.fullStatement ? -1 : 1
