858fd6070cb14ca1d5e7f12eefdea697adb0ae7d
lemon-clown <me.clown.lemon@gmail.com>
2020-04-14 22:24:18 +0800

    :construction:  feat: add package-util

---
+++ packages/package-util/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@barusu/eslint-config'
+  ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json'
+  },
+  rules: {
+  }
+}
