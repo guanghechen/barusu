c8102ec6da2315cdae56520c4b1ba921af2c4a2a
lemon-clown <me.clown.lemon@gmail.com>
2020-06-19 00:36:41 +0800

    :wrench:  chore: update configs for sorting import/export statments

--- packages/rollup-config/src/types/options.ts
+++ packages/rollup-config/src/types/options.ts
@@ -2,7 +2,13 @@ export { EslintOptions } from 'rollup-plugin-eslint'
 export {
   PluginPeerDepsExternalOptions as PeerDepsExternalOptions,
 } from 'rollup-plugin-peer-deps-external'
-export { IOptions as TypescriptOptions } from 'rollup-plugin-typescript2/dist/ioptions'
-export { RollupCommonJSOptions as CommonJSOptions } from '@rollup/plugin-commonjs'
+export {
+  IOptions as TypescriptOptions,
+} from 'rollup-plugin-typescript2/dist/ioptions'
+export {
+  RollupCommonJSOptions as CommonJSOptions,
+} from '@rollup/plugin-commonjs'
 export { RollupJsonOptions as JsonOptions } from '@rollup/plugin-json'
-export { RollupNodeResolveOptions as NodeResolveOptions } from '@rollup/plugin-node-resolve'
+export {
+  RollupNodeResolveOptions as NodeResolveOptions,
+} from '@rollup/plugin-node-resolve'
