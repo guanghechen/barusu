d4f4e9f4d9cbfc638e17e0f4b021ea32238fc45c
lemon-clown <me.clown.lemon@gmail.com>
2020-08-11 22:27:08 +0800

    :construction:  [util-cli] feat: add util func 'installDependencies' (migrated from @barusu/tool-restful-api)

--- tools/restful-api/src/core/init/initializer.ts
+++ tools/restful-api/src/core/init/initializer.ts
@@ -1,13 +1,9 @@
 import execa from 'execa'
 import path from 'path'
-import { isNonExistentOrEmpty } from '@barusu/util-cli'
+import { installDependencies, isNonExistentOrEmpty } from '@barusu/util-cli'
 import { logger } from '../../util/logger'
 import { RestfulApiInitializerContext } from './context'
-import {
-  createInitialCommit,
-  installDependencies,
-  renderTemplates,
-} from './util'
+import { createInitialCommit, renderTemplates } from './util'
 
 
 export class RestfulApiInitializer {
--- tools/restful-api/src/core/init/util.ts
+++ tools/restful-api/src/core/init/util.ts
@@ -38,41 +38,6 @@ export async function renderTemplates(
 
 
 /**
- * Run `npm/yarn install` to Install node.js dependencies
- * @param execaOptions
- */
-export async function installDependencies(execaOptions: execa.Options): Promise<void> {
-  const hasYarnInstalled: boolean = commandExists.sync('yarn')
-
-  /**
-   * If neither yarn nor npm is installed, this operation will be skipped
-   */
-  if (!hasYarnInstalled) {
-    const hasNpmInstalled: boolean = commandExists.sync('npm')
-    if (!hasNpmInstalled) return
-  }
-
-  const { npmScript } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'npmScript',
-      default: hasYarnInstalled ? 'yarn' : 'npm',
-      message: 'npm or yarn?',
-      choices: ['npm', 'yarn', 'skip'],
-      filter: x => toLowerCase(x).trim(),
-      transformer: (x: string) => toLowerCase(x).trim(),
-    },
-  ])
-
-  // skip installing dependencies
-  if (npmScript === 'skip') return
-
-  // install dependencies
-  await execa(npmScript, ['install'], execaOptions)
-}
-
-
-/**
  * Create initial commit
  */
 export async function createInitialCommit(
