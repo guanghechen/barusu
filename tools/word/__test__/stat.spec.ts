679984c38481b1b6c9839f8f88b3370b92381e93
lemon-clown <me.clown.lemon@gmail.com>
2020-09-01 20:36:50 +0800

    :white_check_mark:  [tool-word] test: add test for sub-command 'stat'

---
+++ tools/word/__test__/stat.spec.ts
@@ -0,0 +1,61 @@
+import fs from 'fs-extra'
+import path from 'path'
+import { ColorfulChalkLogger, INFO } from '@barusu/chalk-logger'
+import {
+  COMMAND_NAME,
+  createProgram,
+  execSubCommandStat,
+  logger,
+} from '../src'
+
+
+describe('stat', function () {
+  const caseRootDirectory = path.resolve(__dirname, 'cases', 'files')
+  const kases = fs.readdirSync(caseRootDirectory)
+
+  for (const kase of kases) {
+    const { name: title } = path.parse(kase)
+    const projectDir = caseRootDirectory
+    const filepath = kase
+
+    test(title, async function () {
+      const cliInfos: string[][] = []
+      const collectCliInfos = (...args: any[]) => {
+        cliInfos.push(args.map(x => JSON.stringify(x)))
+      }
+
+      const writeMock = jest
+        .spyOn(logger, 'write')
+        .mockImplementation(collectCliInfos)
+      const consoleLogMock = jest
+        .spyOn(global.console, 'log')
+        .mockImplementation(collectCliInfos)
+
+      const program = createProgram()
+      const args = [
+        '',
+        COMMAND_NAME,
+        'stat',
+        projectDir,
+        '-f',
+        filepath,
+        '--log-level=debug',
+      ]
+
+      await execSubCommandStat(program, args)
+
+      const formattedInfos = cliInfos.map(info => (
+        info.map(text =>
+          text
+          .replace(path.resolve(), '<RootDir>')
+          .replace(/\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}/, '<Date>')
+        )
+      ))
+      expect(formattedInfos).toMatchSnapshot('log info')
+
+      // restore original funcs
+      writeMock.mockRestore()
+      consoleLogMock.mockRestore()
+    })
+  }
+})
