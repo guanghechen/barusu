4962d947442351654b6925800758c32826e27aa8
lemon-clown <me.clown.lemon@gmail.com>
2020-09-14 22:36:56 +0800

    :construction:  [eslint-config] feat: update eslint rules

--- scaffolds/eslint-config/index.js
+++ scaffolds/eslint-config/index.js
@@ -39,31 +39,68 @@ module.exports = {
   ],
   'rules': {
     'class-methods-use-this': 0,
+    'func-call-spacing': ['error', 'never'],
     'func-names': 0,
-    'import/prefer-default-export': 0,
-    'import/no-unresolved': 0,
+    'key-spacing': ['error', { 'align': 'value' }],
     'lines-between-class-members': 0,
+    'new-cap': ['error', { 'newIsCap': true, 'capIsNew': true }],
     'no-await-in-loop': 0,
     'no-bitwise': 0,
     'no-console': 0,
     'no-continue': 0,
-    'no-cond-assign': 0,
-    'no-inner-declarations': 0,
+    'no-cond-assign': ['error', 'always'],
+    'no-inner-declarations': 'error',
+    'no-mixed-operators': 'error',
+    'no-mixed-spaces-and-tabs': 'error',
+    'no-multi-spaces': ['error', { 'ignoreEOLComments': true }],
     'no-param-reassign': ['error', { 'props': true }],
-    'no-plusplus': 0,
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
     'no-restricted-syntax': 0,
-    'no-return-assign': 0,
+    'no-return-assign': ['error', 'always'],
     'no-throw-literal': 0,
     'no-underscore-dangle': 0,
     'prefer-destructuring': 0,
     'quotes': ['error', 'single'],
     'semi': ['error', 'never'],
+    'space-before-blocks': [
+      'error',
+      {
+        'functions': 'always',
+        'keywords': 'always',
+        'classes': 'always',
+      }
+    ],
+    'space-before-function-paren': 0,
+    'spaced-comment': ['error', 'always'],
+    'space-in-parens': ['error', 'never'],
+    'space-infix-ops': ['error', { 'int32Hint': false }],
+    'space-unary-ops': ['error', { 'words': true, 'nonwords': false }],
     '@typescript-eslint/interface-name-prefix': 0,
     '@typescript-eslint/no-empty-function': 0,
     '@typescript-eslint/no-empty-interface': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/no-this-alias': 0,
-    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-this-alias': [
+      'error',
+      {
+        'allowDestructuring': true, // Allow `const { props, state } = this`; false by default
+        'allowedNames': ['self']    // Allow `const self = this`; `[]` by default
+      }
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        'ignoreTypeReferences': true,
+        'typedefs': false,
+      }
+    ],
+    '@typescript-eslint/space-before-function-paren': [
+      'error',
+      {
+        'named': 'never',
+        'anonymous': 'always',
+        'asyncArrow': 'always',
+      }
+    ],
   }
 }
