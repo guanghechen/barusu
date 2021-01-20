4057d4d73378e45a0df7e2bda7fad5d30c29fe03
guanghechen <me.clown.lemon@gmail.com>
2021-01-20 20:56:10 +0800

    :art:  style: format codes

--- packages/chalk-logger/src/logger.ts
+++ packages/chalk-logger/src/logger.ts
@@ -1,7 +1,7 @@
 import type { Writeable } from './types'
 import chalk, { Chalk } from 'chalk'
-import fs from 'fs-extra'
 import dayjs from 'dayjs'
+import fs from 'fs-extra'
 import { inspect } from 'util'
 import { Color, colorToChalk } from './color'
 import { DEBUG, ERROR, FATAL, INFO, Level, VERBOSE, WARN } from './level'
