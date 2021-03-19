f9a257271a6ee47b8e4119fb8302e7d80c902c29
guanghechen <me.guanghechen@gmail.com>
2021-03-19 23:53:43 +0800

    :wrench:  chore: use @guanghechen/jest-config to simplify jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,70 +1,7 @@
-const path = require('path')
-const { compilerOptions } = require('./tsconfig')
-
-const moduleNameMapper = {}
-for (const moduleName of Object.getOwnPropertyNames(compilerOptions.paths)) {
-  const paths = compilerOptions.paths[moduleName].map(p =>
-    path.resolve(__dirname, p),
-  )
-  let pattern = '^' + moduleName.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&') + '$'
-  moduleNameMapper[pattern] = paths.length === 1 ? paths[0] : paths
-}
+const { tsMonorepoConfig } = require('@guanghechen/jest-config')
 
 module.exports = {
-  bail: true,
-  verbose: true,
-  errorOnDeprecated: true,
-  roots: ['<rootDir>/src', '<rootDir>/__test__'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper,
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json',
-    },
-  },
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '/(__test__)/[^/]+\\.spec\\.tsx?$',
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/coverage/',
-    '/lib/',
-    '/dist/',
-    '/build/',
-    '/target/',
-    '/vendor/',
-    '/release/',
-    '/example/',
-    '/demo/',
-    '/doc/',
-    '/tmp/',
-    '/__tmp__/',
-    '/script/',
-  ],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/src/**/*.{js,jsx,ts,tsx}',
-    '**/src/*.{js,jsx,ts,tsx}',
-    '!**/src/cli.ts',
-    '!**/src/command/_util.ts',
-    '!**/test/cases/**',
-    '!**/__test__/cases/**',
-    '!**/node_modules/**',
-    '!**/lib/**',
-    '!**/dist/**',
-    '!**/build/**',
-    '!**/target/**',
-    '!**/vendor/**',
-    '!**/release/**',
-    '!**/example/**',
-    '!**/demo/**',
-    '!**/doc/**',
-    '!**/tmp/**',
-    '!**/__tmp__/**',
-    '!**/script/**',
-    '!**/coverage/**',
-  ],
+  ...tsMonorepoConfig(__dirname),
   coverageThreshold: {
     global: {
       branches: 60,
@@ -73,5 +10,4 @@ module.exports = {
       statements: 60,
     },
   },
-  coverageReporters: ['text', 'text-summary'],
 }
